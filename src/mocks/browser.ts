a8e191d052595a2097d3583dfe1e07bc0b43ae38
Deyan Slavov <didkoslawow@gmail.com>
2025-05-11 14:40:48 +0300

    Fix MSW type compatibility issue

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,4 +1,5 @@
 import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 
-export const worker = setupWorker(...handlers);
+// Cast the handlers to any to avoid type incompatibilities between MSW versions
+export const worker = setupWorker(...(handlers as any));
